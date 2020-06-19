5245bfe213a507091559274eed774878960a7cdc
Onion-Ring <bb1702@g.skku.edu>
2020-06-19 23:42:14 +0900

    graph_final

--- subway.c
+++ subway.c
@@ -1,55 +1,21 @@
-#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 100
-
+#define MAX_SIZE 10000
+#define INT_MAX 999999999
 
 
 int matrix2[MAX_SIZE][MAX_SIZE];
 int matrix[MAX_SIZE][MAX_SIZE];
-int link[100][30] = { 0, };
 int top2 = 0;
 int distance[MAX_SIZE];
 int found[MAX_SIZE];
 int n = MAX_SIZE;
 
 int top = 0;
-typedef struct transfer* pointer;
-
-pointer stack[MAX_SIZE];
-pointer stack2[MAX_SIZE];
-
-typedef struct transfer {
-	int name;
-	pointer next;
-};
-
-void init() {
-	for (int i = 0; i < MAX_SIZE; i++) {
-		stack[i] = (pointer)malloc(sizeof(*stack[0]));
-	}
-}
-
-void transpate() {
-	for (int i = 0; i < MAX_SIZE; i++) {
-		stack2[i] = (pointer)malloc(sizeof(*stack2[0]));
-		stack2[i] = stack[i];
-	}
-}
 
 
-//push 함수 손보기
-void push(int a, int b) {
-	/* showing a,b connected */
-	pointer k_next = (pointer)malloc(sizeof(*k_next));
-	stack[a]->name = a;
-	k_next = stack[a]->next;
-	k_next->name = b;
-	
-	stack[a] = stack[a]->next;
-
-}
 int choose() { 
 	/* find smallest distance not yet checked */ 
 	int i, min, minpos; 
@@ -63,9 +29,7 @@ int choose() {
 	return minpos; 
 }
 
-void shortestPath(int v,int n2){
-	init();
-	transpate();
+void shortestPath(int v){
 
 	int i, u, w;
 	for (i = 0; i < n; i++) {
@@ -81,35 +45,12 @@ void shortestPath(int v,int n2){
 		for (w = 0; w < n; w++)
 			if (!found[w])
 				if (distance[u] + matrix[u][w] < distance[w]) {
-					push(u, w);
 					distance[w] = distance[u] + matrix[u][w];
 					matrix[v][w] = distance[w];
 					matrix[w][v] = matrix[v][w];
 				}
 	}
 }
-
-
-
-
-
-
-
-// 1호선	25 서면 26 부전 29 연산 30 교대 31 동래 (1~40 : 1호선)
-// 2호선	45 백스코 48 수영 59 서면 67 사상 73 덕천 (41~83 : 2호선)
-// 3호선	84 수영 88 연산 89 거제 92 미남 96 덕천 100 대저 (84~100 : 3호선)
-// 4호선	101 미남 102 동래 (101~114 : 4호선)
-// 5호선	115 사상 121 대저 (109~135 : 부산김해(5호선))
-// 6호선	136 부전 138 거제 139 교대 146 백스코 (136~151 : 동해선(6호선))
-
-
-
-//1호선		서면 동래
-//2호선		백스코 덕진
-//3호선		수영 대저
-//4호선		미남 안평
-//5호선		
-
 
 int speculate(int p) {
 	int sum = 0;
@@ -126,19 +67,26 @@ int speculate(int p) {
 }
 
 main() {
+	FILE* fp;
+	fp = fopen("./input.txt","r");
 	int i = 0;
 	int j = 0;
 	int n1, n2;
 	for (i=0; i < MAX_SIZE; i++) {
 		for (j = 0; j < MAX_SIZE; j++) {
-			scanf("%d", &matrix[i][j]);
+			fscanf(fp,"%d", &matrix[i][j]);
 			matrix2[i][j] = matrix[i][j];
 			//printf("abcdffsfasdlkfjaldfkjaef");
 		}
 	}
+	/*for (i = 0; i < MAX_SIZE; i++) {
+		shortestPath(i);
+	}*/
 
-	printf("\n출발지와 목적지 선택\n");
+	printf("\npress first and final station\n");
 	scanf("%d %d", &n1, &n2);
+	getchar();
+	shortestPath(n1);
 	/*for (i=0; i < MAX_SIZE; i++) {
 		for (j = 0; j < MAX_SIZE; j++) {
 			printf("%d ", matrix[i][j]);
@@ -147,26 +95,28 @@ main() {
 	}*/
 
 
-	for (i = 0; i < MAX_SIZE; i++) {
-		if (i == n1) {
-			shortestPath(i,n2);
+	
+	printf("%d subway\n", matrix[n1][n2]);
+
+	printf("press c to continue\nor anything to finish\n");
+	char a;
+	//rewind(stdin);
+	//rewind(fp);
+	while (1) {
+		scanf("%c", &a);
+		if (a == 'c') {
+			printf("\npress first and final station\n");
+			scanf("%d %d", &n1, &n2);
+			shortestPath(n1);
+			printf("%d subway\n", matrix[n1][n2]);
+			getchar();
+			printf("press c to continue\nor anything to finish\n");
 		}
 		else {
-			shortestPath(i,0);
+			break;
 		}
 		
 	}
-	i = 0;
-	printf("%d정거장 지남\n", matrix[n1][n2]);
-
-	while (link[i]) {
-		printf("%d-\t", link[i]);
-		if (speculate(i) == 1)
-			printf("\t환승역 아님\n");
-		else
-			printf("\t환승역\n");
-		
-		i++;
-	}
+	return;
 
 }
